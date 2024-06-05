fa996fdff48fffa88a6f163141b1ce8287c0778f
JiHawn <ghks4861@kookmin.ac.kr>
2024-06-05 17:32:45 +0900

    Add SshKey Struct

--- ssh_key/ssh_key.go
+++ ssh_key/ssh_key.go
@@ -1,131 +1,72 @@
 package sshkey
 
 import (
-	"context"
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"os"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	"golang.org/x/crypto/ssh"
 )
 
 var KeyDir = os.Getenv("HOME") + "/.ssh/cloud-basic"
 
-func createPrivateKey(keyName string) error {
+type SSHKey struct {
+	PrivateKey *rsa.PrivateKey
+	PublicKey  ssh.PublicKey
+}
 
-	// key가 저장되는 디렉토리 생성
-	if err := os.MkdirAll(KeyDir, 0755); err != nil {
-		return err
-	}
-
-	// 빈 파일 생성
-	privateKeyPath := KeyDir + "/" + keyName
-	privateFile, err := os.Create(privateKeyPath)
-	if err != nil {
-		return err
-	}
-	defer privateFile.Close()
-
-	// rsa key 생성
+func NewSshKey() (*SSHKey, error) {
+	key := &SSHKey{}
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		return err
-	}
-
-	// rsa key를 pem 형식으로 인코딩
-	privatePem := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-	}
-
-	// pem 형식의 rsa key를 파일에 쓰기
-	if err := pem.Encode(privateFile, privatePem); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func removePrivateKey(keyName string) error {
-	keyPath := KeyDir + "/" + keyName
-	if err := os.Remove(keyPath); err != nil {
-		return err
-	}
-	return nil
-}
-
-func createPublicKey(keyName string) error {
-	privateKeyPath := KeyDir + "/" + keyName
-	privateKeyFile, err := os.ReadFile(privateKeyPath)
-	if err != nil {
-		return err
-	}
-
-	privateKeyBlock, _ := pem.Decode(privateKeyFile)
-	if privateKeyBlock == nil {
-		return errors.New("failed to decode private key")
-	}
-
-	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return os.WriteFile(privateKeyPath+".pub", ssh.MarshalAuthorizedKey(publicKey), 0644)
+	key.PrivateKey = privateKey
+	key.PublicKey = publicKey
+
+	return key, nil
 }
 
-func removePublicKey(keyName string) error {
-	keyPath := KeyDir + "/" + keyName + ".pub"
-	if err := os.Remove(keyPath); err != nil {
-		return err
-	}
-	return nil
-}
+// func applyKeyToContainer(cli *client.Client, containerID, keyName string) error {
+// 	publicKeyPath := KeyDir + "/" + keyName + ".pub"
+// 	publicKeyFile, err := os.ReadFile(publicKeyPath)
+// 	if err != nil {
+// 		return err
+// 	}
 
-func applyKeyToContainer(cli *client.Client, containerID, keyName string) error {
-	publicKeyPath := KeyDir + "/" + keyName + ".pub"
-	publicKeyFile, err := os.ReadFile(publicKeyPath)
-	if err != nil {
-		return err
-	}
+// 	publicKey := string(publicKeyFile)
 
-	publicKey := string(publicKeyFile)
+// 	execFunc := func(cmd []string) error {
+// 		_, err := cli.ContainerExecCreate(
+// 			context.Background(),
+// 			containerID,
+// 			types.ExecConfig{
+// 				Cmd: cmd,
+// 			},
+// 		)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		return nil
+// 	}
 
-	execFunc := func(cmd []string) error {
-		_, err := cli.ContainerExecCreate(
-			context.Background(),
-			containerID,
-			types.ExecConfig{
-				Cmd: cmd,
-			},
-		)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
+// 	if err := execFunc([]string{"mkdir", "-p", "/root/.ssh"}); err != nil {
+// 		return err
+// 	}
 
-	if err := execFunc([]string{"mkdir", "-p", "/root/.ssh"}); err != nil {
-		return err
-	}
+// 	if err := execFunc([]string{"sh", "-c", "echo " + publicKey + " >> /root/.ssh/authorized_keys"}); err != nil {
+// 		return err
+// 	}
 
-	if err := execFunc([]string{"sh", "-c", "echo " + publicKey + " >> /root/.ssh/authorized_keys"}); err != nil {
-		return err
-	}
+// 	if err := execFunc([]string{"chmod", "600", "/root/.ssh/authorized_keys"}); err != nil {
+// 		return err
+// 	}
 
-	if err := execFunc([]string{"chmod", "600", "/root/.ssh/authorized_keys"}); err != nil {
-		return err
-	}
-
-	return nil
-}
+// 	return nil
+// }
--- ssh_key/ssh_key_test.go
+++ ssh_key/ssh_key_test.go
@@ -1,126 +1,20 @@
 package sshkey
 
 import (
-	"os"
 	"testing"
-
-	"github.com/PaulOh5/cloud-basic/instance"
-	"github.com/docker/docker/client"
-	"golang.org/x/crypto/ssh"
 )
 
-func TestCreateKey(t *testing.T) {
-	testCases := []struct {
-		keyName string
-	}{
-		{keyName: "paul"},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.keyName, func(t *testing.T) {
-			err := createPrivateKey(tc.keyName)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer removePrivateKey(tc.keyName)
-
-			if _, err := os.Stat(KeyDir + "/" + tc.keyName); os.IsNotExist(err) {
-				t.Fatalf("ssh private key not created")
-			}
-
-			err = createPublicKey(tc.keyName)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer removePublicKey(tc.keyName)
-
-			if _, err := os.Stat(KeyDir + "/" + tc.keyName + ".pub"); os.IsNotExist(err) {
-				t.Fatalf("ssh public key not created")
-			}
-		})
-	}
-}
-
-func TestApplyKeyToContainer(t *testing.T) {
-	testCases := []struct {
-		keyName string
-	}{
-		{keyName: "paul"},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.keyName, func(t *testing.T) {
-			cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer cli.Close()
-
-			config := instance.ContainerConfig{
-				JupyterPort: "9999",
-				SshPort:     "2222",
-			}
-			containerID, err := instance.CreateContainer(cli, config)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer instance.RemoveContainer(cli, containerID)
-
-			err = createPrivateKey(tc.keyName)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer removePrivateKey(tc.keyName)
-
-			err = createPublicKey(tc.keyName)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer removePublicKey(tc.keyName)
-
-			err = applyKeyToContainer(cli, containerID, tc.keyName)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if err := checkKeyAppliedToContainer(tc.keyName, config); err != nil {
-				t.Fatalf("failed to apply key to container: %v", err)
-			}
-		})
-	}
-}
-
-func checkKeyAppliedToContainer(keyName string, containerConfig instance.ContainerConfig) error {
-	keyPath := KeyDir + "/" + keyName
-	keyFile, err := os.ReadFile(keyPath)
+func TestSshKey(t *testing.T) {
+	key, err := NewSshKey()
 	if err != nil {
-		return err
+		t.Fatal(err)
 	}
 
-	signer, err := ssh.ParsePrivateKey(keyFile)
-	if err != nil {
-		return err
+	if key.PrivateKey == nil {
+		t.Fatal("private key not created")
 	}
 
-	sshConfig := &ssh.ClientConfig{
-		User: "root",
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	if key.PublicKey == nil {
+		t.Fatal("public key not created")
 	}
-
-	sshClient, err := ssh.Dial("tcp", "localhost:"+containerConfig.SshPort, sshConfig)
-	if err != nil {
-		return err
-	}
-	defer sshClient.Close()
-
-	session, err := sshClient.NewSession()
-	if err != nil {
-		return err
-	}
-	defer session.Close()
-
-	return nil
 }
